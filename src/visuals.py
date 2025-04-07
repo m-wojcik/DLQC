54efc50ddf9dcac63c71f0837d93cf160959f8b5
m-wojcik <michuwoj.wojcik@gmail.com>
2025-04-07 18:20:46 +0200

    Work control works, new elements of the loss function

--- src/visuals.py
+++ src/visuals.py
@@ -38,7 +38,7 @@ def plotBlochSphere():
 
     return fig, ax
 
-def plotBlochSphereTrajectory(trajx, trajy, trajz, nframes, title='bloch.mp4', writer='ffmpeg'):
+def animateBlochSphereTrajectory(trajx, trajy, trajz, nframes, title='bloch.mp4', writer='ffmpeg'):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.set(xlim3d=[-1.2,1.2], ylim3d=[-1.2,1.2], zlim3d=[-1.2,1.2])
@@ -102,4 +102,48 @@ def plotBlochSphereTrajectory(trajx, trajy, trajz, nframes, title='bloch.mp4', w
     ani = animation.FuncAnimation(fig=fig, func=update, fargs=(line,), frames=nframes, interval=30)
     ani.save(filename=title, writer=writer)
 
+
+def plotBlochSphereTrajectory(trajx, trajy, trajz, nframes, title='bloch.mp4', writer='ffmpeg'):
+    fig = plt.figure(figsize=(8,8))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set(xlim3d=[-1.2,1.2], ylim3d=[-1.2,1.2], zlim3d=[-1.2,1.2])
+    npoints = 100
+
+    r = 1
+    u = np.linspace(0,2*np.pi, npoints)
+    v = np.linspace(0, np.pi, npoints)
+    x = r * np.outer(np.cos(u), np.sin(v))
+    y = r * np.outer(np.sin(u), np.sin(v))
+    z = r * np.outer(np.ones(np.size(u)), np.cos(v))
+
+    ax.plot_surface(x,y,z, color='linen', alpha=0.4)
+
+    theta = np.linspace(0, 2*np.pi, npoints)
+    straight = np.zeros(npoints)
+    round1 = r * np.sin(theta)
+    round2 = r * np.cos(theta)
+
+    ax.plot(round1, round2, straight, color='black', alpha=0.75)
+    ax.plot(straight, round1, round2, color='black', alpha=0.75)
+    #ax.plot(round1, straight, round2, color='black', alpha=0.75)
+
+    line = np.linspace(-r,r,npoints)
+
+    ax.plot(line, straight, straight, color='black', alpha=0.6)
+    ax.plot(straight, line, straight, color='black', alpha=0.6)
+    ax.plot(straight, straight, line, color='black', alpha=0.6)
+    ax.set_box_aspect((np.ptp(x), np.ptp(y), np.ptp(z)))
+    
+    line = ax.plot(trajx, trajy, trajz, lw=3)
+
+    ar = ax.quiver(0, 0, 0, # <-- starting point of vector
+            trajx[-1], trajy[-1], trajz[-1], # <-- directions of vector
+            color = 'black', alpha = 1, lw = 3,
+            arrow_length_ratio = 0.15)
+    
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+
+    return fig, ax
 
